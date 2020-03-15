d016c520df66932efe35848cdfe5d452f05bec00
ashok kumar <ashok.b@bitlasoft.com>
2020-03-15 12:19:41 +0530

    basic_setup

---
+++ lib/bulk_update.rb
@@ -0,0 +1,9 @@
+require "bulk_update/version"
+
+module BulkUpdate
+  class Error < StandardError
+  end
+  class Update
+    
+  end
+end
---
+++ lib/bulk_update/version.rb
@@ -0,0 +1,3 @@
+module BulkUpdate
+  VERSION = "0.1.0"
+end
